47761fb3100bd1e69881b6f4627702bc3509019d
jimschenchen <jimschenchen@163.com>
2020-02-12 15:05:45 -0500

    spectre + 注册组件

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -4,6 +4,22 @@
             <ul class="menu-tab">
                 <li v-for="item in menuTab" :key="item.id" :class="{'current':item.current}" @click="toggleMenu(item)">{{item.txt}}</li>
             </ul>
+
+            <form>
+                <div class="form-group" :class="this.form.mail.error">
+                    <label class="form-label">邮箱</label>
+                    <input class="form-input" type="text" placeholder="邮箱" v-model="form.mail.content">
+                    <p class="form-input-hint">{{form.mail.hint}}</p>
+                </div>
+
+                <div class="form-group" :class="this.form.pass.error">
+                    <label class="form-label">密码</label>
+                    <input class="form-input" type="text" placeholder="密码" v-model="form.pass.content">
+                    <p class="form-input-hint">{{form.pass.hint}}</p>
+                </div>
+
+                <button class="btn btn-primary" @click="submit">登录</button>
+            </form>
         </div>
     </div>
 </template>
@@ -17,14 +33,45 @@ export default {
                 { txt: "login", current: true},
                 { txt: "register", current: false},
             ], 
+            form: {
+                mail: {content: '', error: '', hint: ''},
+                pass: {content: '', error: '', hint: ''}
+            }
         }  
     },
     methods: {
-        toggleMenu(data){
+        toggleMenu(data) {
             this.menuTab.forEach(element => {
                 element.current = false
             });
             data.current = true;
+        },
+        submit() {
+            // valide mail
+            let reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+            if (!this.form.mail.content) {
+                this.form.mail.hint = '邮箱不能为空!';
+                this.form.mail.error = 'has-error';
+            } else if (!reg.test(this.form.mail.content)) {
+                this.form.mail.hint = '请输入正确的邮箱!';
+                this.form.mail.error = 'has-error';
+            } else {
+                this.form.mail.hint = '邮箱正确！';
+                this.form.mail.error = 'has-success';
+            }
+
+            // valide pass
+            let reg2 = /^(?!\D+$)(?![^a-zA-Z]+$)\S{8,20}$/;
+            if (!this.form.pass.content) {
+                this.form.pass.hint = '密码不能为空!';
+                this.form.pass.error = 'has-error';
+            } else if (!reg2.test(this.form.pass.content)) {
+                this.form.pass.hint = '请输入8-20位数字+字母的密码！';
+                this.form.pass.error = 'has-error';
+            } else {
+                this.form.pass.hint = '密码正确！';
+                this.form.pass.error = 'has-success';
+            }
         }
     }
 }
@@ -59,5 +106,10 @@ export default {
 }
 .current {
     background-color: rgba(0, 0, 0, .1)
+}
+
+.btn 
+{
+    width: 100%;
 }
 </style>
